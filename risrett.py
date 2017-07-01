eab1437682ffc199ee2228ef26f77ffe68ae76f7
LeeM24 <emamorra@alice.it>
2017-07-01 14:45:40 +0200

    Add files via upload
    Piccoli programmi creati da me

---
+++ risrett.py
@@ -0,0 +1,24 @@
+b = 4
+h = 6
+#ho inserito i dati
+area = b*h
+perimetro = b*2+h*2
+
+
+if b == 0:
+	print ("la base torna 0")
+	print ("impossibile completare")
+else:
+	print ("area:")
+	print area
+	print ("perimetro:")
+	print perimetro
+if h == 0:
+	print ("l'altezza torna 0")
+	print ("impossibile completare")
+else:
+		print ("ecco l'area e perimetro del rettangolo")
+		
+
+
+
