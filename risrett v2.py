2d84d18527e4e0604a118ee30a44136d4e58bc77
LeeM24 <emamorra@alice.it>
2017-07-02 10:44:00 +0200

    Add files via upload
    New version of  risrett.

---
+++ risrett v2.py
@@ -0,0 +1,24 @@
+b = input("inserisci valore base: ")
+h = input("inserisci valore altezza: ")
+#ho inserito i dati
+area = b*h
+perimetro = b*2+h*2
+
+
+if b == 0:
+	print ("valore base = 0")
+	print ("impossibile completare")
+else:
+	print ("area:")
+	print area
+	print ("perimetro:")
+	print perimetro
+if h == 0:
+	print ("valore altezza = 0")
+	print ("impossibile completare")
+else:
+		print ("ecco l'area e perimetro del rettangolo")
+		
+
+
+
