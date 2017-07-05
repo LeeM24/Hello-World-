5a3a9ce2570762dd6bb2c5b99dde49f396a8d2a0
LeeM24 <emamorra@alice.it>
2017-07-05 17:25:43 +0200

    Update Implrisrett.py

--- Implrisrett.py
+++ Implrisrett.py
@@ -1,21 +1,24 @@
+
 nome = 'Emanuele'
 codicePersonale = 2003
 autenticazione = input("inserire codice personale: ")
 if autenticazione == 2003:
 	print ("Accesso efettuato con successo!")
 else: 
-	print("Per quale motivo vuoi enttare nel mio account!? ")
+	print("Per quale motivo vuoi entrare nel mio account!? ")
 	print("Accesso negato!")
+	exit()
 
 b = input("inserisci la base: ")
 h = input("inserisci l'altezza: ")
 
 if b == 0:
 	print("base = 0")
+	exit()
 	
 if h == 0:
 	print("altezza = 0")
-	
+	exit()
 area = b*h
 perimetro = b*2+h*2
 
