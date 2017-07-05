bd41063602ee19eddaa4d6dfeb855d01d949783e
LeeM24 <emamorra@alice.it>
2017-07-05 17:31:40 +0200

    Delete login.py

--- login.py
+++
@@ -1,7 +0,0 @@
-nome = 'Emanuele'
-codicePersonale = 2003
-autenticazione = input("inserire codice personale: ")
-if autenticazione == 2003:
-	print ("Accesso eefettuato con successo!")
-else:
-	print("Per quale motivo vuoi enttare nel mio account!? ")
